920ea3e2c36a3dd7861526fb1649938a1a98a961
Alexandar Quirk <184129704+axqr@users.noreply.github.com>
2024-10-08 00:05:35 +0530

    Update build.zig

--- build.zig
+++ build.zig
@@ -1,11 +1,5 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const target = b.resolveTargetQuery(.{
-        .cpu_arch = .wasm32,
-        .os_tag = .freestanding,
-    });
-
     const install_site = b.addInstallDirectory(.{
         .source_dir = b.path("src/www"),
         .install_dir = .prefix,
