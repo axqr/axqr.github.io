c9449148703a3ebd1878a83de884054f5301d668
Alexandar Quirk <184129704+axqr@users.noreply.github.com>
2024-10-07 23:46:55 +0530

    Create common.js

---
+++ src/www/common.js
@@ -0,0 +1,51 @@
+function page(stub, href) {
+  return { stub, href };
+}
+
+const PAGES = [
+  page('home', '/index.html'),
+  page('resume', '/blog.html'),
+  page('demos', '/links.html'),
+  page('blog', '/about.html'),
+];
+
+/**
+ * create a button which links to something
+ */
+function linkButton(text, href) {
+  const button = document.createElement('button');
+
+  button.classList.add('header-link')
+  button.innerText = text;
+  button.onclick = () => {
+    window.location.href = href;
+  };
+
+  return button;
+}
+
+/**
+ * generate common header contents
+ */
+function genHeader(div) {
+  div.textContent = '';
+
+  PAGES
+    .map(({stub, href}) => linkButton(stub, href))
+    .forEach((child) => div.appendChild(child));
+}
+
+/**
+ * generate common footer contents
+ */
+function genFooter(div) {
+  div.textContent = '';
+}
+
+addEventListener('load', () => {
+  const header = document.querySelector('#header');
+  const footer = document.querySelector('#footer');
+
+  genHeader(header);
+  genFooter(footer);
+});
